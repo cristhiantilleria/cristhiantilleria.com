d1ab9f6a6ceb217df154dd1aa3df2a5703c5c13b
Cristhian Benitez <cristhiantilleria@gmail.com>
2025-11-23 20:39:53 -0500

    refactor: reorganize imports and enhance the About page content; update styling and remove unused sections

--- app/about/page.js
+++ app/about/page.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import styles from './about.module.css';
-import Image from 'next/image';
-import { BsArrowRight } from 'react-icons/bs';
-import Link from 'next/link';
 import PageWrapper from '@/components/pagewrapper';
-import { getPlaiceholder } from 'plaiceholder';
 import fs from 'fs';
+import Image from 'next/image';
+import Link from 'next/link';
 import path from 'path';
+import { getPlaiceholder } from 'plaiceholder';
+import { BsArrowRight } from 'react-icons/bs';
+import styles from './about.module.css';
 
 async function getBlurData() {
   const { base64 } = await getPlaiceholder(
@@ -45,30 +44,18 @@ export default async function About() {
             <BsArrowRight />
             &nbsp; Instagram
           </Link>
-          <Link target="blank" href="https://www.linkedin.com/in/cristhiantilleria/">
-            <BsArrowRight />
-            &nbsp; LinkedIn
-          </Link>
-          <Link target="blank" href="https://cristhiantilleria.tumblr.com/">
-            <BsArrowRight />
-            &nbsp; Tumblr
-          </Link>
         </div>
       </section>
       <section className={styles.about__column} grid-col="2">
         <p className={styles.about__headline}>
-          Hey! I'm Cristhian, a New York based Freelance Developer & Designer. My interests lays in
-          the intersection of design and the web. I love to think about the value of design in the
-          work I do.
-          <br />
+          New York based designer, developer and{' '}
+          <a href="https://www.pipebenitez.com" style={{ color: 'inherit', textDecoration: 'underline' }}>
+            photographer
+          </a>
+          .
         </p>
-        <p className={styles.about__headline}>
-          {/* ADD LINKS TO PLAYLISTS,BOOKS AND PHOTOS INSIDE WORDS */}
-          On my time off you can find me reading books, thinking about a new personal project,
-          enjoying music, learning some new language on duolingo or capturing moments through
-          photography.
-        </p>
-        <div className={styles.about__services}>
+        <p className={styles.about__headline}>For inquiries send me a message through email or social media.</p>
+        {/* <div className={styles.about__services}>
           <ul>
             <li style={{ fontWeight: '500', marginBottom: '.5rem' }}>Services</li>
             <li>Web Development</li>
@@ -77,7 +64,7 @@ export default async function About() {
             <li>Brand Identity</li>
             <li>Logo Design</li>
           </ul>
-        </div>
+        </div> */}
       </section>
     </PageWrapper>
   );
